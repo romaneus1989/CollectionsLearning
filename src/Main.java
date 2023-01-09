c6f027b184b53a626f4b49db24624f50f803658d
romaneus1989 <rom-danil@yandex.ru>
2023-01-09 14:34:12 +0500

    ArrayList example with cats completed

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,20 @@ public class Main {
         for (Cat cat : cats){
             catsList.add(cat);
         }
-        catsList.add(new Cat("Бегемот"));
-        System.out.println(catsList.toString());
+        catsList.add(new Cat("Бегемот2"));
+
+        catsList.remove(1);
+        Cat cat = catsList.get(0);
+        System.out.println(cat);
+        System.out.println(catsList.indexOf(cat));
+        catsList.add(2,cat);
+        catsList.set(2,new Cat("Меня сюда вставили"));
+
+        catsList.removeAll(Arrays.asList(cat, catsList.get(3)));
+
+        System.out.println(catsList.size());
+
+
 
     }
 }
